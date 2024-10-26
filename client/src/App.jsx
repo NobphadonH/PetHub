b64b0d78eabd7b0e14c9c4f0373cc75d7ef7256d
KaEnds <kenza.ke15@gmail.com>
2024-10-26 13:40:00 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,85 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-6xl font-bold text-black">
+      <span className='text-orange'>welcome</span> pethub website
+    </h1>
+    <img className='my-10 mx-auto' src="https://images.unsplash.com/photo-1544568100-847a948585b9?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="test image" width="500" height="600"/>
+    <div className='w-3/5 mx-auto'>
+      <h1 className='text-3xl text-left my-5'>heading</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem, esse molestiae amet sit dolore perferendis voluptate quo cupiditate, fuga itaque pariatur soluta consequuntur ut deleniti unde quam fugiat aspernatur? Labore.</p>
+    </div>
+
+    <h1 className='text-5xl mt-40 text-red-600'>test deployment</h1>
+
+    <p className='mt-10'>test daisy ui</p>
+
+    <div className='w-3/5 mx-auto py-10'>
+    <label className="input input-bordered flex items-center gap-2">
+    <input type="text" className="grow" placeholder="Search" />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      fill="currentColor"
+      className="h-4 w-4 opacity-70">
+      <path
+        fillRule="evenodd"
+        d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+        clipRule="evenodd" />
+    </svg>
+    </label>
+    <label className="input input-bordered flex items-center gap-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        fill="currentColor"
+        className="h-4 w-4 opacity-70">
+        <path
+          d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+        <path
+          d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+      </svg>
+      <input type="text" className="grow" placeholder="Email" />
+    </label>
+    <label className="input input-bordered flex items-center gap-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        fill="currentColor"
+        className="h-4 w-4 opacity-70">
+        <path
+          d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+      </svg>
+      <input type="text" className="grow" placeholder="Username" />
+    </label>
+    <label className="input input-bordered flex items-center gap-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        fill="currentColor"
+        className="h-4 w-4 opacity-70">
+        <path
+          fillRule="evenodd"
+          d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+          clipRule="evenodd" />
+      </svg>
+      <input type="password" className="grow" value="password" />
+    </label>
+    </div>
+    <div>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
+    </>
+  )
+}
+
+export default App
