b6d70ad7f92c509cfba86d62e3c5d73dc8e4dcbb
NopeNH <nopz3456z@gmail.com>
2024-10-31 23:12:38 +0700

    Server init / paste sign in-out

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,138 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const signin = async (req, res) => {
+    try {
+      const { username, password, brokerID } = req.body;
+      dbpool.getConnection(async (err, connection) => {
+        if (err) throw err;
+        connection.query(
+          "SELECT * FROM Users WHERE Username = ? AND BrokerID = ?",
+          [username, brokerID],
+          async (err, rows) => {
+            if (err) throw err;
+            const user = rows[0];
+  
+            //console.log(rows)
+            //connection.release()
+            if (!user) {
+              return res
+                .status(400)
+                .json({ error: "Invalid username or password or brokerID" });
+            }
+  
+            // Compare passwords
+            const isPasswordCorrect = await bcrypt.compare(
+              password,
+              user["Password"]
+            );
+  
+            if (!isPasswordCorrect) {
+              return res
+                .status(400)
+                .json({ error: "Invalid username or password or brokerID" });
+            }
+  
+            // Login successful, generate token and set cookie
+            //constgenerateTokenAndSetCookie(user['UserID'], res); // Call the function with userId
+  
+            const userID = user["UserID"];
+  
+            // Respond with user information
+            const token = jwt.sign({ userID }, "Bhun-er", {
+              expiresIn: "15d",
+            });
+  
+            console.log("login success");
+            res
+              .cookie("jwt", token, {
+                maxAge: 15 * 24 * 60 * 60 * 1000, // MS
+                httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+                sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+                secure: process.env.NODE_ENV !== "development",
+              })
+              .status(200)
+              .json({ token });
+          }
+        );
+      });
+    } catch (error) {
+      console.log("Error in login controller", error.message);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
+  
+  // signout controller not finish wait for kennn
+  export const signout = (req, res) => {
+    try {
+      console.log(req);
+      res.cookie("user-auth", "", { maxAge: 0 });
+      res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+      console.log("Error in logout controller", error.message);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
+  
+  export const signinStaff = (req, res) => {
+    try {
+      const { username, password } = req.body;
+      dbpool.getConnection(async (err, connection) => {
+        if (err) throw err;
+        connection.query(
+          "SELECT * FROM Broker_Staffs WHERE Username = ? ",
+          [username],
+          async (err, rows) => {
+            if (err) throw err;
+            const staff = rows[0];
+  
+            console.log(rows);
+            //connection.release()
+            if (!staff) {
+              return res
+                .status(400)
+                .json({ error: "Invalid username or password " });
+            }
+  
+            // Compare passwords
+            const isPasswordCorrect = await bcrypt.compare(
+              password,
+              staff["Password"]
+            );
+  
+            if (!isPasswordCorrect) {
+              console.log("wrong password");
+              console.log(staff["Password"]);
+              return res
+                .status(400)
+                .json({ error: "Invalid username or password" });
+            }
+  
+            // Login successful, generate token and set cookie
+            //constgenerateTokenAndSetCookie(user['UserID'], res); // Call the function with userId
+  
+            const staffID = staff["StaffID"];
+  
+            // Respond with user information
+            const token = jwt.sign({ staffID }, "Bhun-er-staff", {
+              expiresIn: "15d",
+            });
+  
+            console.log("login success");
+            res
+              .cookie("jwt", token, {
+                maxAge: 15 * 24 * 60 * 60 * 1000, // MS
+                httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+                sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+                secure: process.env.NODE_ENV !== "development",
+              })
+              .status(200)
+              .json({ token });
+          }
+        );
+      });
+    } catch (error) {
+      console.log("Error in login controller", error.message);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
