8c9d73e75cc74efc20d9cdf82dd207a1fd443a7a
NopeNH <nopz3456z@gmail.com>
2024-11-02 00:04:45 +0700

    update dbConnection.js

--- server/utils/dbConnection.js
+++ server/utils/dbConnection.js
@@ -4,43 +4,38 @@ import { sshConfig } from '../config/sshConfig.js';
 import { dbConfig } from '../config/dbConfig.js';
 
 export async function connectToDatabase() {
-    const sshClient = new Client();
-  
-    const sshTunnelConfig = {
-      srcHost: '127.0.0.1',  // Localhost
-      srcPort: 3306,         // MySQL default port
-      dstHost: dbConfig.host, // RDS endpoint
-      dstPort: 3306          // RDS MySQL port
-    };
-  
-    return new Promise((resolve, reject) => {
-      sshClient.on('ready', () => {
-        sshClient.forwardOut(
-          sshTunnelConfig.srcHost,
-          sshTunnelConfig.srcPort,
-          sshTunnelConfig.dstHost,
-          sshTunnelConfig.dstPort,
-          (err, stream) => {
-            if (err) {
-              sshClient.end();
-              reject(new Error('SSH Tunnel creation failed'));
-            }
-  
-            const connection = mysql.createPool({
-              ...dbConfig,
-              stream
-            });
-  
-            connection.connect(error => {
-              if (error) {
-                reject(error);
-              } else {
-                console.log('Connected to RDS through SSH tunnel');
-                resolve({ connection, sshClient });
-              }
-            });
+  const sshClient = new Client();
+
+  const sshTunnelConfig = {
+    srcHost: '127.0.0.1',  // Localhost
+    srcPort: 3306,         // MySQL default port
+    dstHost: dbConfig.host, // RDS endpoint
+    dstPort: 3306          // RDS MySQL port
+  };
+
+  return new Promise((resolve, reject) => {
+    sshClient.on('ready', () => {
+      sshClient.forwardOut(
+        sshTunnelConfig.srcHost,
+        sshTunnelConfig.srcPort,
+        sshTunnelConfig.dstHost,
+        sshTunnelConfig.dstPort,
+        (err, stream) => {
+          if (err) {
+            sshClient.end();
+            reject(new Error('SSH Tunnel creation failed'));
           }
-        );
-      }).connect(sshConfig);
-    });
-  }
+
+          // Use connection pool without calling `.connect()`
+          const pool = mysql.createPool({
+            ...dbConfig,
+            stream
+          });
+
+          console.log('Connected to RDS through SSH tunnel');
+          resolve({ pool, sshClient });
+        }
+      );
+    }).connect(sshConfig);
+  });
+}
