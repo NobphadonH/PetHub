b390fb9ff1c94fb2ba65c00345df340f358d7422
Nobphadon Hankittikanjana <nopz3456z@gmail.com>
2024-12-06 00:14:18 +0700

    fix hotel approve box [final commit]

--- pethub-website/src/components/HotelApproveBox.jsx
+++ pethub-website/src/components/HotelApproveBox.jsx
@@ -22,7 +22,7 @@ function HotelApproveBox({
     const handleApprove =  async (e) => {
       e.preventDefault()
       try {
-        const res = await axios.post("http://localhost:5000/api/hotel/updateHotelVerification", {hotelID: hotelObj.hotelID, verification: "verfify"}, { withCredentials:true})
+        const res = await axios.post("http://localhost:5000/api/hotel/updateHotelVerification", {hotelID: hotelObj.hotelID, verification: "verified"}, { withCredentials:true})
         setActionCnt(++actionCnt);
         console.log(res);
         if (res.status == 200) {
