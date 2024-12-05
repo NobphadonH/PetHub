5e0dbe06780dfb33e474a46f7ad3741fc1c09b11
Nobphadon Hankittikanjana <nopz3456z@gmail.com>
2024-12-05 21:39:27 +0700

    edit vite config

--- pethub-website/vite.config.js
+++ pethub-website/vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
   ssr: {
     noExternal: ["remix-utils"],
   },
+  server: {
+    host: '0.0.0.0',  // Listen on all network interfaces
+    port: 5173,        // Ensure it's the port you're using
+  }
 })
