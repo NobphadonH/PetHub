5d6e2ebb3936e4e92ab4c3583781ff1aee8632eb
Nobphadon Hankittikanjana <nopz3456z@gmail.com>
2024-12-06 02:52:10 +0700

    change to anywhere

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
